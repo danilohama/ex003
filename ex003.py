22a848e3822a735fec6d79af749d3bc375552408
Danilo ohama <91641703+danilohama@users.noreply.github.com>
2023-09-04 19:20:14 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,4 @@
+n1 = int(input('\033[037mDigite um numero\033[0m: '))
+n2 = int(input('\033[037mDigite o segundo numero:\033[0m '))
+soma = n1 + n2
+print('A soma entre  \033[032m{}\033[0m e \033[032m{}\033[0m vale \033[032m{}\033[0m'.format(n1, n2, soma))
